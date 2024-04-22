b05e5564a81169277ca553c883eed22b68a15b22
Phoenixstars1111 <160893450+Phoenixstars1111@users.noreply.github.com>
2024-04-22 16:37:19 +0100

    color

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-<script>
-    const scrollContainer = document.querySelector('.scroll-container');
+const scrollContainer = document.querySelector('.scroll-container');
 
-    function scrollImages(scrollAmount) {
-      scrollContainer.scrollBy({
-        left: scrollAmount,
-        behavior: 'smooth'
-      });
-    }
-  </script>
+function scrollImages(scrollAmount) {
+  scrollContainer.scrollBy({
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+}
